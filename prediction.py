d195e9ffdb5ed756eb8966e41442df75e2059a4d
ghulvasu <ghulvasu069@gmail.com>
2025-10-04 19:12:32 +0530

    updated prediction.py file

--- prediction.py
+++ prediction.py
@@ -1,42 +1,53 @@
-import nltk
 import string
 from nltk.corpus import stopwords
+import nltk
 from nltk.stem.porter import PorterStemmer
+
+# --- Download NLTK data for deployment ---
+# This checks if the data is present and downloads it if not.
+try:
+    nltk.data.find('tokenizers/punkt')
+except nltk.downloader.DownloadError:
+    nltk.download('punkt', quiet=True)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except nltk.downloader.DownloadError:
+    nltk.download('stopwords', quiet=True)
+# -----------------------------------------
 
 ps = PorterStemmer()
 
 def transform_text(text):
     """
     Performs text preprocessing:
-    1. Converts to lowercase
-    2. Tokenizes
-    3. Removes non-alphanumeric characters
-    4. Removes stop words and punctuation
-    5. Stems the words
+    1. Lowercases the text.
+    2. Tokenizes the text into words.
+    3. Removes non-alphanumeric characters.
+    4. Removes stop words and punctuation.
+    5. Applies stemming to each word.
+    Returns the processed text as a single string.
     """
-    # Convert to lowercase
     text = text.lower()
-
-    # Tokenize the text
     text = nltk.word_tokenize(text)
 
-    # Remove non-alphanumeric characters
-    alphanumeric_chars = []
+    y = []
     for i in text:
         if i.isalnum():
-            alphanumeric_chars.append(i)
+            y.append(i)
 
-    # Remove stop words and punctuation
-    text = alphanumeric_chars[:]
-    processed_text = []
+    text = y[:]
+    y.clear()
+
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
-            processed_text.append(i)
+            y.append(i)
 
-    # Stem the words
-    text = processed_text[:]
-    stemmed_text = []
+    text = y[:]
+    y.clear()
+
     for i in text:
-        stemmed_text.append(ps.stem(i))
+        y.append(ps.stem(i))
 
-    return " ".join(stemmed_text)
+    return " ".join(y)
+
